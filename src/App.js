112f196cbb78952ecad39cd28d9c0535f10198e9
Janet Cook <monickercoding@gmail.com>
2020-08-11 10:04:01 -0600

    initial commit for assessment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Game from './components/game/Game';
+import rulyrooster from './assets/images/rulyrooster.png';
+import outFoxed from './assets/images/outFoxed.png'
+
+import '../src/App.css'
 
 function App() {
+  const roosters = {
+      name: 'Rulyville Roosters',
+      logoSrc: rulyrooster
+  }
+
+  const foxes = {
+      name: 'Funkytown Foxes',
+      logoSrc: outFoxed
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+          <Game
+              venue="Rulyville Arena"
+              homeTeam={roosters}
+              visitingTeam={foxes}
+          />
+
+          <Game
+              venue="Funkytown Center"
+              homeTeam={foxes}
+              visitingTeam={roosters}
+          />
+      </div>
+  )
 }
 
+
 export default App;
+
+// Clipart images courtesy of: https://clipartix.com/fox-clipart-image-256/ and
+// https://clipartix.com/rooster-clip-art-image-32293/ and https://images.freeimages.com/images/premium/previews/3937/39372318-basket-ball-net.jpg
+
+// Audio sound credits courtesy of: https://www.freesoundeffects.com/free-track/bounce-1-468901/
+// https://www.freesoundeffects.com/free-track/swish-468906/
+
